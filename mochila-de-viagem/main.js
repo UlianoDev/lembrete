33e426587bd7a4ca8dc07de710fcaf27868293b8
UlianoDev <118945166+UlianoDev@users.noreply.github.com>
2023-03-30 16:18:42 -0300

    Add files via upload

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -1,20 +1,50 @@
 const form = document.getElementById("novoItem");
-const lista = document.querySelector('.lista');
+const lista = document.querySelector(".lista");
+const lembretes = JSON.parse(localStorage.getItem("Lembretes")) || [];
 
-form.addEventListener('submit', (e)=> {
-  e.preventDefault();
-  let nome = e.target.elements.nome.value;
-  let quantidade = e.target.elements.quantidade.value;
-  //<li class="item"><strong>7</strong>Camisas</li>
-  console.log(e);
+lembretes.forEach((elemento) => {
+  adicionaLista(elemento);
+});
+
+function adicionaLista(memoriaLocal) {
   const li = document.createElement("li");
   const strong = document.createElement("strong");
-  li.classList.add("item");
   //li.innerHTML = `<strong>${quantidade}</strong>${nome}`;
-  strong.innerHTML += quantidade;
-  li.appendChild(strong);
-  li.innerHTML += nome;
-  lista.appendChild(li);
 
-  console.log(li);
-})
+  li.classList.add("item");
+  li.appendChild(strong);
+  strong.innerHTML += memoriaLocal.quantidade;
+  strong.dataset.id = memoriaLocal.id;
+  li.innerHTML += memoriaLocal.nome;
+  lista.appendChild(li);
+}
+function adicionaStorage(memoriaLocal) {
+  lembretes.push(memoriaLocal);
+
+  localStorage.setItem("Lembretes", JSON.stringify(lembretes));
+  //lembretes.push(JSON.stringify(memoriaLocal));
+  //localStorage.setItem("Lembretes", lembretes);
+}
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  let nome = evento.target.elements.nome.value;
+  let quantidade = evento.target.elements.quantidade.value;
+  let id = lembretes.length;
+  let memoriaLocal = {
+    nome: nome,
+    quantidade: quantidade,
+    id: id,
+  };
+  const existe = lembretes.find((elementoAtual) => {
+    return elementoAtual.nome === nome;
+  });
+
+  if (existe) {
+    existe.id = memoriaLocal.id;
+  } else {
+    //memoriaLocal.id = id;
+
+    adicionaLista(memoriaLocal);
+    adicionaStorage(memoriaLocal);
+  }
+});
