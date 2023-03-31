90cc4ac00c1d891acd1935fd5e7965806b02702a
UlianoDev <118945166+UlianoDev@users.noreply.github.com>
2023-03-31 13:27:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const lista = document.querySelector(".lista");
 const lembretes = JSON.parse(localStorage.getItem("Lembretes")) || [];
 let id = lembretes.length > 0 ? lembretes[lembretes.length - 1].id + 1 : 0;
 
-
-lembretes.forEach(element => {
-  adicionaLista(element)
+lembretes.forEach((element) => {
+  adicionaLista(element);
 });
 
 function adicionaLista(memoriaLocal) {
@@ -15,20 +14,36 @@ function adicionaLista(memoriaLocal) {
 
   const li = document.createElement("li");
   const strong = document.createElement("strong");
+  const botao = document.createElement("button");
 
   //li.innerHTML = `<strong>${quantidade}</strong>${nome}`;
   li.classList.add("item");
   li.appendChild(strong);
   strong.innerHTML += memoriaLocal.quantidade;
-  strong.dataset.id = memoriaLocal.id
+  strong.dataset.id = memoriaLocal.id;
   li.innerHTML += memoriaLocal.nome;
-
+  li.appendChild(botao);
+  botao.innerText = "X";
+  removeBotao(botao, memoriaLocal.id);
   lista.appendChild(li);
+}
+function removeBotao(botao, id) {
+  botao.addEventListener("click", function () {
+    const liBotao = this.parentNode;
+    liBotao.remove();
+    removeBotaoStorage(id);
+  });
+}
 
-  
+function removeBotaoStorage(id) {
+  lembretes.splice(
+    lembretes.findIndex((element) => element.id === id),
+    1
+  );
+  localStorage.setItem("Lembretes", JSON.stringify(lembretes));
+  console.log(lembretes);
 }
 function adicionaStorage(memoriaLocal) {
-
   lembretes.push(memoriaLocal);
   //localStorage.setItem("Lembretes", JSON.stringify(lembretes));
   //lembretes.push(JSON.stringify(memoriaLocal));
@@ -42,8 +57,8 @@ function atualizaLista(idExistente, quantidadeAtual) {
   quantidadeAtual = parseInt(quantidadeAtual);
 
  quantidadeIdExistente += quantidadeAtual;*/
- document.querySelector(`[data-id='${idExistente}']`).innerHTML = quantidadeAtual
-
+  document.querySelector(`[data-id='${idExistente}']`).innerHTML =
+    quantidadeAtual;
 }
 /*function atualizaStorage(idExistente, quantidadeAtual) {
   let objetoExistente = parseInt(lembretes.find((elemento)=> elemento.id === idExistente).quantidade)
@@ -59,56 +74,46 @@ function atualizaLista(idExistente, quantidadeAtual) {
      console.log(typeof quantidadeIdExistente);
 
     //localStorage.setItem('Lembretes', JSON.stringify()}*/
-  
-
-  
-   
 
 form.addEventListener("submit", (evento) => {
-
-    evento.preventDefault();
+  evento.preventDefault();
   let nome = evento.target.elements.nome.value;
   let quantidade = evento.target.elements.quantidade.value;
-    let memoriaLocal = {
-      nome: nome,
-      quantidade: quantidade,
-      id: id
-    };
-  
-    const existe = lembretes.find(element => {
-      return element.nome === memoriaLocal.nome})
+  let memoriaLocal = {
+    nome: nome,
+    quantidade: quantidade,
+    id: id,
+  };
 
+  const existe = lembretes.find((element) => {
+    return element.nome === memoriaLocal.nome;
+  });
 
+  if (existe) {
+    let idExistente = existe.id;
+    let quantidadeAtual = memoriaLocal.quantidade;
+    memoriaLocal.id = existe.id;
+    //console.log(quantidadeAtual);
+    //let quantidadeIdExistente = parseInt(document.querySelector(
+    //  `[data-id='${idExistente}']`
+    //).innerText)
+    //atualizaLista(idExistente, quantidadeAtual);
+    // quantidadeAtual =
+    atualizaLista(idExistente, quantidadeAtual);
+    //console.log(quantidadeAtual)
+    //atualizaStorage(idExistente, quantidadeAtual);
 
+    //atualizaStorage(idExistente, quantidadeAtual);
+    //console.log(quantidadeIdExistente);
+    //console.log(memoriaLocal);
+    //console.log(lembretes[idExistente]);
+    lembretes[idExistente] = memoriaLocal;
+    //console.log(lembretes[idExistente]);
+  } else {
+    adicionaLista(memoriaLocal);
+    adicionaStorage(memoriaLocal);
+    id = id + 1;
+  }
 
-      if(existe){
-        let idExistente = existe.id 
-        let quantidadeAtual = memoriaLocal.quantidade
-        memoriaLocal.id = existe.id
-        //console.log(quantidadeAtual);
-        //let quantidadeIdExistente = parseInt(document.querySelector(
-        //  `[data-id='${idExistente}']`
-        //).innerText)
-        //atualizaLista(idExistente, quantidadeAtual);
-       // quantidadeAtual = 
-       atualizaLista(idExistente, quantidadeAtual);
-        //console.log(quantidadeAtual)
-        //atualizaStorage(idExistente, quantidadeAtual);
-
-       //atualizaStorage(idExistente, quantidadeAtual);
-                //console.log(quantidadeIdExistente);
-                console.log(memoriaLocal)
-                console.log(lembretes[idExistente])
-                 lembretes[idExistente] = memoriaLocal
-                console.log(lembretes[idExistente]);
-      
-      }else{  
-      adicionaLista(memoriaLocal);
-      adicionaStorage(memoriaLocal);
-          id = id + 1;
-        }
-
-          localStorage.setItem("Lembretes", JSON.stringify(lembretes));
-
-
+  localStorage.setItem("Lembretes", JSON.stringify(lembretes));
 });
